1f84e6a3687d9844878309179fd7a7ba0280cbc2
lopo <lopo@zju.edu.cn>
2023-11-02 23:58:37 +0800

    feat: template

---
+++ src/client.ts
@@ -0,0 +1,14 @@
+/**
+ * The base class of all rope clients.
+ */
+abstract class RopeClient {
+
+}
+
+class RopeClientSameOrigin extends RopeClient {
+
+}
+
+class RopeClientCrossOrigin extends RopeClient {
+
+}
