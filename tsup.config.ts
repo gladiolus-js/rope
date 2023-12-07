72e3d702656c7dc725924b87c4e6e296c5f4b8d9
lopo <lopo@zju.edu.cn>
2023-12-07 17:12:49 +0800

    feat: config worker file

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     clean: true,
     outDir: 'bundle',
+    publicDir: 'worker',
     entry: [ 'src/index.ts' ],
     dts: true,
-    format: [ 'cjs', 'esm' ]
+    format: [ 'cjs', 'esm' ],
 })
