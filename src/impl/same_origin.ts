c8cf7ac9618dd44def387fc5ceb138cd16782389
lopo <lopo@zju.edu.cn>
2023-12-07 17:06:20 +0800

    feat: RopeClientSameOrigin impl

--- src/impl/same_origin.ts
+++ src/impl/same_origin.ts
@@ -6,6 +6,12 @@ import { REvMessage } from "../common/event";
  * A rope client used in the same origin situation.
  *
  * This will use `SharedWorker` to communicate with each other.
+ *
+ * ---
+ * @param id The id of the client
+ * @param strategy The strategy of the client
+ * @param handler The handler to handle incoming messages.
+ * @param onRejected The handler to handle the rejection of the client. (required when `strategy` is `'respect'`)
  */
 class RopeClientSameOrigin<MessageIn = unknown, MessageOut = MessageIn> extends RopeClient<MessageIn, MessageOut> {
     /**
@@ -14,17 +20,28 @@ class RopeClientSameOrigin<MessageIn = unknown, MessageOut = MessageIn> extends 
      */
     #worker: SharedWorker
 
+    #onRejected?: VoidFunction
+
     /**
      * The proxy function to handle incoming messages from the `SharedWorker` instance.
      * @private
      */
     #handlerProxy(ev: MessageEvent<REvMessage<MessageIn>>) {
-        // TODO: schedule the incoming message
-        console.log('received message from worker:', ev.data)
+        const rEv = ev.data
+
+        if(rEv.evName === 'duplicate' && rEv.message) {
+            this.#onRejected?.()
+        } else if(rEv.evName === 'message') {
+            this.handler?.(rEv.message)
+        }
     }
 
-    constructor(id: string, strategy: RopeClientStrategy = 'respect', handler: ((ev: MessageIn) => void) | null = null) {
+    constructor(id: string, strategy: 'respect', handler: ((ev: MessageIn) => void) | null, onRejected: VoidFunction);
+    constructor(id: string, strategy: 'plunder', handler: ((ev: MessageIn) => void) | null);
+    constructor(id: string, strategy: RopeClientStrategy = 'respect', handler: ((ev: MessageIn) => void) | null = null, onRejected?: VoidFunction) {
         super(id, strategy, handler);
+
+        this.#onRejected = onRejected
 
         const sw = new SharedWorker(RopeConfig.workerURL)
         this.#worker = sw
